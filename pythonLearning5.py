e5289546540bbc7e7ea383bd3bba1a1a7a60da3a
徐化睿 <101656396+Xvhuarui@users.noreply.github.com>
2024-05-31 15:21:54 +0800

    Create pythonLearning5.py

---
+++ pythonLearning5.py
@@ -0,0 +1,16 @@
+# 黑马程序员全套Python教程_Python基础入门视频教程，零基础小白自学Python入门教程
+# https://www.bilibili.com/video/BV1o4411M71o?p=1&vd_source=8fd2de1d80aed33dc7cfc2afe1e694a0
+
+# P72 07-循环应用1:1-100偶数累加和方法1
+i = 1
+T = 0
+while i <= 100:
+    if i % 2 == 0:
+        T += i
+    i += 1
+print(T)
+
+# 先明确需求，然后撰写代码
+
+# P75 10-了解break和continue
+# break-终止此循坏 continue-退出当前一次循环继而执行下一次循环代码
